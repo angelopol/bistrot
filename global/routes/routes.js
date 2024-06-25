2b9710c71a13ad84debaf10edd0bcb897fa1a140
angelopol <114349306+angelopol@users.noreply.github.com>
2024-06-24 21:10:07 -0400

    Mantenimientos folder added.

--- global/routes/routes.js
+++ global/routes/routes.js
@@ -7,6 +7,7 @@ import { createInventarioRouter } from '../../Modules/Inventario/routes/inventar
 import { createVentasRouter } from '../../Modules/ventas/routes/ventas.js'
 import { createComprasRouter } from '../../Modules/compras/routes/compras.js'
 import { createCocinaRouter } from '../../Modules/cocina/routes/cocina.js'
+import { createMantenimientoRouter } from '../../Modules/mantenimiento/routes/mantenimiento.js'
 
 export const routes = ({app}) => {
     app.use('/movies', createMovieRouter())
@@ -18,4 +19,5 @@ export const routes = ({app}) => {
     app.use('/ventas', createVentasRouter())
     app.use('/compras', createComprasRouter())
     app.use('/cocina', createCocinaRouter())
+    app.use('/mantenimiento', createMantenimientoRouter())
 }
