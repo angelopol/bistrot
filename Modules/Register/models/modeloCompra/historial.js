c3e3f2485a82f711b5ff3ca429ae03df094bbfb9
JoSantana0005 <joseasantana05@gmail.com>
2024-06-29 14:46:28 -0400

    Se realiza la conexion a la base de datos bistrot en los modelos del modulo de compras

---
+++ Modules/Register/models/modeloCompra/historial.js
@@ -0,0 +1,14 @@
+// Agregando la conexion a a base de datos
+
+import mysql from 'mysql2/promise'
+import 'dotenv/config'
+
+const DBConfig = {
+  host: '127.0.0.1' || process.env.DB_HOST,
+  user: 'root' || process.env.DB_USERNAME,
+  port: 3306 || process.env.DB_PORT,
+  password: '' || process.env.DB_PASSWORD,
+  database: 'bistrot' || process.env.DB_DATABASE,
+}
+
+const connection = await mysql.createConnection(DBConfig)
