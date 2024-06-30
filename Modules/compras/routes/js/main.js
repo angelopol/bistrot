75ad2b53141c71a39f34515d7ca3689a7a0d7321
PepoTH <cesardarizaleta@gmail.com>
2024-06-30 12:17:15 -0400

    Agregando js

---
+++ Modules/compras/routes/js/main.js
@@ -0,0 +1,4 @@
+tabla = document.getElementById('tabla-registro')
+//Tabla Contiene un thead y un tbody, tbody vacio, rellenar
+tbody = document.getElementById('tabla-registro-body')
+//Por si es necesario, directamente el body
