3a172e6db0e870c6251869ef0d8a81bfc4932496
PepoTH <cesardarizaleta@gmail.com>
2024-06-30 12:19:59 -0400

    Agregando objetos al js => Main

--- Modules/compras/routes/js/main.js
+++ Modules/compras/routes/js/main.js
@@ -1,4 +1,12 @@
-tabla = document.getElementById('tabla-registro')
+registro = document.getElementById('tabla-registro')
 //Tabla Contiene un thead y un tbody, tbody vacio, rellenar
-tbody = document.getElementById('tabla-registro-body')
+regisbody = document.getElementById('tabla-registro-body')
+//Por si es necesario, directamente el body
+
+saldo = document.getElementById('saldo')
+//Saldo a modificar
+
+solicitudes = document.getElementById('tabla-soli')
+//Tabla Contiene un thead y un tbody, tbody vacio, rellenar
+solibody = document.getElementById('tabla-soli-body')
 //Por si es necesario, directamente el body
