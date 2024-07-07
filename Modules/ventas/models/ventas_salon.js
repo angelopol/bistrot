035202e625d25802d89eef20eebd7f5759ec294b
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-07-07 13:46:09 -0400

    add funcion update models ventas, submodulo caja

--- Modules/ventas/models/ventas_salon.js
+++ Modules/ventas/models/ventas_salon.js
@@ -53,17 +53,80 @@ export class VentasModel {
 
     static async create_caja({input}) {
         const {
-            turno_horario, tasa_del_dia, apertura, cierra, monto_inicial, monto_final, ingresos, egresos
+            turno_horario, tasa_del_dia, apertura, cierre, monto_inicial, monto_final, ingresos, egresos
         } = input
 
         try {
             await connection.query(
-                "INSERT INTO submodulo_caja (turno_horario, tasa_del_dia, apertura, cierra, monto_inicial, monto_final, ingresos, egresos) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", [turno_horario, tasa_del_dia, apertura, cierra, monto_inicial, monto_final, ingresos, egresos]
+                "INSERT INTO submodulo_caja (turno_horario, tasa_del_dia, apertura, cierra, monto_inicial, monto_final, ingresos, egresos) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", [turno_horario, tasa_del_dia, apertura, cierre, monto_inicial, monto_final, ingresos, egresos]
             )
         } catch (error) {
             throw new Error("Error creando una venta")
         }
         
+    }
+
+    static async update({id , input}) {
+        const {
+            turno_horario, tasa_del_dia, apertura, cierre, monto_inicial, monto_final, ingresos, egresos
+        } = input;
+
+        try {
+            // Construir la parte de la consulta SQL para actualizar
+            let updates = [];
+            let values = [];
+
+            if (turno_horario) {
+                updates.push("turno_horario = ?");
+                values.push(turno_horario);
+            }
+            if (tasa_del_dia) {
+                updates.push("tasa_del_dia = ?");
+                values.push(tasa_del_dia);
+            }
+            if (apertura) {
+                updates.push("apertura = ?");
+                values.push(apertura);
+            }
+            if (cierre) {
+                updates.push("cierre = ?");
+                values.push(cierre);
+            }
+            if (monto_inicial) {
+                updates.push("monto_inicial = ?");
+                values.push(monto_inicial);
+            }
+            if (monto_final) {
+                updates.push("monto_final = ?");
+                values.push(monto_final);
+            }
+            if (ingresos) {
+                updates.push("ingresos = ?");
+                values.push(ingresos);
+            }
+            if (egresos) {
+                updates.push("egresos = ?");
+                values.push(egresos);
+            }
+
+            // Comprobar si hay campos para actualizar
+            if (updates.length == 0) {
+                throw new Error("No se proporcionaron datos para actualizar.");
+            }
+
+            // Agregar el ID como último valor en el array de values
+            values.push(id);
+
+            // Consulta SQL para actualizar
+            const query = `UPDATE submodulo_caja SET ${updates.join(', ')} WHERE id = ?`;
+            const [result] = await connection.query(query, values);
+            
+        } catch (error) {
+            console.error('Error al actualizar la venta:', error);
+            throw error;
+        } finally {
+            await connection.end();
+        }
     }
 
 
