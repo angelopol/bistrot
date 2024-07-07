5179bfb59e79643c9ea700cde6dfd41c8dda6346
JoSantana0005 <joseasantana05@gmail.com>
2024-07-07 13:47:37 -0400

    Se agrega la etiqutea script al html y se prueba la logica del saldo

--- Modules/compras/routes/js/main.js
+++ Modules/compras/routes/js/main.js
@@ -1,12 +1,27 @@
-registro = document.getElementById('tabla-registro')
+let registro = document.getElementById('tabla-registro')
 //Tabla Contiene un thead y un tbody, tbody vacio, rellenar
-regisbody = document.getElementById('tabla-registro-body')
+let regisbody = document.getElementById('tabla-registro-body')
 //Por si es necesario, directamente el body
 
-saldo = document.getElementById('saldo')
+let saldo = document.getElementById('saldo')
 //Saldo a modificar
 
-solicitudes = document.getElementById('tabla-soli')
+let solicitudes = document.getElementById('tabla-soli')
 //Tabla Contiene un thead y un tbody, tbody vacio, rellenar
-solibody = document.getElementById('tabla-soli-body')
+let solibody = document.getElementById('tabla-soli-body')
 //Por si es necesario, directamente el body
+
+//Modificacion del saldo
+//Dependiendo de las solicitudes que se haga el saldo van a ir disminuyendo
+
+function disminuirSaldo(costo){
+    const saldoNum = parseInt(saldo.textContent)
+    const resultado = parseInt(saldoNum - costo)
+    return resultado
+}
+let result = disminuirSaldo(1000)
+//Funcionalidad del boton compra
+let comprar = document.getElementById("btn-compra").addEventListener('click',() =>{
+    saldo.innerHTML = result
+
+})
