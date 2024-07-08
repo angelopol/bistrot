8db9da0863d0bf7d02efada632ad4bd3d61fc6c7
Manuel Nava <manueldavidnava31@gmail.com>
2024-07-07 21:20:03 -0400

    Ajustes Pedidos

--- views/ventas/Vista_Pedidos/pedido_logica.js
+++ views/ventas/Vista_Pedidos/pedido_logica.js
@@ -1,11 +1,36 @@
-const abrir_modal = document.querySelector("#button-abrir-modal");
-const cerrar_modal = document.querySelector("#button-cerrar-modal");
-const modal = document.querySelector("#modal");  
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Selecciona todos los botones de abrir modal
+    const openModalButtons = document.querySelectorAll('.button-abrir-modal');
 
-abrir_modal.addEventListener("click",()=>{
-    modal.showModal();
-})
+    // Asigna el evento click a cada botón de abrir modal
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Encuentra el modal correspondiente al botón clicado
+            const modal = button.nextElementSibling;
+            if (modal && modal.tagName.toLowerCase() === 'dialog') {
+                modal.showModal();
+            }
+        });
+    });
 
-cerrar_modal.addEventListener("click",()=>{
-    modal.close()
-})
+    // Selecciona todos los botones de cerrar modal
+    const closeModalButtons = document.querySelectorAll('.button-cerrar-modal');
+
+    // Asigna el evento click a cada botón de cerrar modal
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Encuentra el modal correspondiente al botón clicado
+            const modal = button.closest('dialog');
+            if (modal) {
+                modal.close();
+            }
+        });
+    });
+
+    // Cierra la ventana modal al hacer clic fuera de ella
+    document.addEventListener('click', (event) => {
+        if (event.target.tagName.toLowerCase() === 'dialog') {
+            event.target.close();
+        }
+    });
+});
