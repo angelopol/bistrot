3db7652327db47bee8b79599017365716f4ccde4
alejandrochmejia <alejandrochmejia@gmail.com>
2024-07-09 12:48:47 -0400

    enlazando principal-reportes

--- Modules/mantenimiento/version_2/Conexion_pagina.js
+++ Modules/mantenimiento/version_2/Conexion_pagina.js
@@ -1,5 +1,6 @@
 const navegar_contacto = document.getElementById('Page_contactos');
 const navegar_personal = document.getElementById('Page_personal');
+const navegar_reportes = document.getElementById('Page_reportes');
 
 navegar_contacto.addEventListener('click',function(){
     window.location.href = 'Pagina_contacto.html';
@@ -7,4 +8,8 @@ navegar_contacto.addEventListener('click',function(){
 
 navegar_personal.addEventListener('click',function(){
     window.location.href = 'Pagina_personal.html';
+})
+
+navegar_reportes.addEventListener('click',function(){
+    window.location.href = 'Pagina_reportes.html';
 })
