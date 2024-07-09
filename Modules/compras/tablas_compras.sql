8e7f5ec798f2b26d32dd4bfd4f1c2815a53347ec
Samuel <samuelmolina664@gmail.com>
2024-07-09 13:03:55 -0400

    Cambios en las tablas de la base de datos

--- Modules/compras/tablas_compras.sql
+++ Modules/compras/tablas_compras.sql
@@ -25,21 +25,23 @@ SELECT * FROM bistrot.historial;
 -- Tabla de los Productos de las compras
 CREATE TABLE `bistrot`.`ProductoCompras` (
     `ID_Producto` INT NOT NULL AUTO_INCREMENT, -- ID del Producto
-    `ID_Proveedor` INT NOT NULL, -- ID del proveedor
+    -- `ID_Proveedor` INT NOT NULL, -- ID del proveedor
     `NombreP` VARCHAR(200) NOT NULL, -- Nombre del producto
     `Unidades` VARCHAR(200) NOT NULL, -- Unidades de Compra (EJ: bultos)
     `Consumo` VARCHAR(200) NOT NULL, -- Unidades de Consumo (EJ: kg)
     -- `Monto` FLOAT NOT NULL, -- Monto de producto
     -- `Numero_Factura` INT NOT NULL, -- Numero de factura
     PRIMARY KEY (`ID_Producto`),
-    UNIQUE (`ID_Proveedor`, `NombreP`)
+    UNIQUE ( `NombreP`)
 );
 
-INSERT INTO bistrot.ProductoCompras (ID_Proveedor, NombreP, Unidades, Consumo)
+DROP TABLE bistrot.productocompras;
+
+INSERT INTO bistrot.ProductoCompras (NombreP, Unidades, Consumo)
 VALUES
-(1, 'Café Colombiano', 'bultos', 'kg'),
-(2, 'Arroz', 'bultos', 'kg'),
-(3, 'Pasta', 'bultos', 'Kg');
+('Café Colombiano', 'bultos', 'kg'),
+('Arroz', 'bultos', 'kg'),
+('Pasta', 'bultos', 'Kg');
 
 SELECT * FROM bistrot.ProductoCompras;
 
@@ -67,9 +69,10 @@ CREATE TABLE `bistrot`.`Solicitudes` (
 	`ID_Requisicion` INT NOT NULL AUTO_INCREMENT, -- Identificación de la requisición
     `Departamento` VARCHAR(200) NOT NULL, -- Departamento
     `ID_Empleado` INT NOT NULL , -- ID de los empleados
-    `Codigo_Producto` INT NOT NULL, -- Código de los productos
+    `Cantidad` FLOAT NOT NULL, -- Cantidad de los productos
     `Nombre_Producto` VARCHAR(200) NOT NULL, 
     `FECHA` DATETIME NOT NULL, -- Fecha en la que se hizo la solicitud
+    `Aprobada` boolean not null, -- Valor booleano para identificar si esta aprobada o no
     `DETALLE` TEXT(255) , -- Justificación
     PRIMARY KEY (`ID_Requisicion`),
     UNIQUE (`ID_Requisicion`, `FECHA`)
