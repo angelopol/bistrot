491ef032541bceb0430ceb75514a0fdccde425e0
Juan Wu <juanwu8238@gmail.com>
2024-07-09 12:20:13 -0400

    Version 2 montada, explicacion en txt

---
+++ Modules/mantenimiento/version_2/Conexion_pagina.js
@@ -0,0 +1,10 @@
+const navegar_contacto = document.getElementById('Page_contactos');
+const navegar_personal = document.getElementById('Page_personal');
+
+navegar_contacto.addEventListener('click',function(){
+    window.location.href = 'Pagina_contacto.html';
+})
+
+navegar_personal.addEventListener('click',function(){
+    window.location.href = 'Pagina_personal.html';
+})
