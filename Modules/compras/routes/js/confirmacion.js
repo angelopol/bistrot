ba41c11c5bfebbfafc74eca0297fed94eb2b3926
PepoTH <cesardarizaleta@gmail.com>
2024-07-08 20:48:20 -0400

    Confirmacion Terminada

---
+++ Modules/compras/routes/js/confirmacion.js
@@ -0,0 +1,26 @@
+//Textos donde Ingresar los datos
+producto = document.getElementById('producto')
+fecha = document.getElementById('fecha')
+cantidad = document.getElementById('cantidad')
+precio = document.getElementById('precio')
+//Saldos Previo - Actual
+previo = document.getElementById('previo')
+actual = document.getElementById('actual')
+//numero de Orden
+orden = document.getElementById('orden')
+
+
+let exit = document.getElementsByClassName('title-exit')[0]
+//Boton de Exit
+
+exit.onclick = function(){
+    location.href = '../html/compra.html'
+}
+
+
+//Boton de Regresar
+let btn_exit = document.getElementById('btn-regis')
+
+btn_exit.onclick = function(){
+    location.href = '../html/compra.html'
+}
