3e7cbd38748ad4ee980bf8b5936942c360174c48
JoSantana0005 <joseasantana05@gmail.com>
2024-07-10 10:33:05 -0400

    Colocando las validaciones de la tabla historial

---
+++ Modules/Register/schemas/ValidacionHistorial.js
@@ -0,0 +1,11 @@
+import z from "zod"
+
+export const HistorialValidaschema = z.object({
+    FECHA: z.date(),
+    Pago: z.string(),
+    Producto: z.string(),
+    Cantidad: z.number().nonnegative("La cantidad no se puede ser un numero negativo"),
+    Precio: z.number().nonnegative("El precio no puede ser un numero negativo"),
+    Nombre_Proveedor: z.string(),
+    Autorizacion: z.string()
+})
