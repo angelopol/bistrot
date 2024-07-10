ab68335435d38fa7624829406a816b4a4880ffc4
alejandrochmejia <alejandrochmejia@gmail.com>
2024-07-10 08:24:33 -0400

    agregando boton de eliminar reporte

--- Modules/mantenimiento/version_2/JSReporte.js
+++ Modules/mantenimiento/version_2/JSReporte.js
@@ -1,4 +1,4 @@
-function mostrarVista(boton){
+function mostrarVista(boton){ //Funcion que se ejecuta a la hora de mostrar una vista de un reporte
     // Obtener la fila del botón
     var fila = boton.parentNode.parentNode;
 
@@ -15,25 +15,30 @@ function mostrarVista(boton){
     vista = document.getElementById("vistaReportes");
     form = document.getElementById("formReportes");
     botonAgregar = document.getElementById("Button_formulario_Agregar");
+    botonEliminar = document.getElementById("Button_formulario_EliminarReporte");
 
     //Cerrando ventana principal de reportes y abriendo vista del reporte
     if (form.style.display == "block"){
         form.style.display = "none"
         botonAgregar.style.display = "none"
+        botonEliminar.style.display = "inline"
         vista.style.display = "block"
     }
 }
 
-function regresar(){
+function regresar(){ //Funcion que se ejecuta con el boton de Regresar
     //Obtecion de elementos
     vista = document.getElementById("vistaReportes");
     form = document.getElementById("formReportes");
+    botonEliminar = document.getElementById("Button_formulario_EliminarReporte");
+    botonAgregar = document.getElementById("Button_formulario_Agregar");
 
     //Comprobar que elemento esta abierto para ejecutar la accion
     if(form.style.display=="block"){
         window.location.href = 'Pagina_principal.html';
     }else if(vista.style.display=="block"){
         vista.style.display = "none"
+        botonEliminar.style.display = "none"
         botonAgregar.style.display = "inline"
         form.style.display = "block"
     }
