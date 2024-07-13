6483bed9a763c49f8a9fd805aaef9d6ee2ae4ee4
alemt19 <alfredomt1906@gmail.com>
2024-07-13 17:16:59 -0400

    Se comenta el scriptInstrumentos

--- Modules/cocina/views/scriptInstrumentos.js
+++ Modules/cocina/views/scriptInstrumentos.js
@@ -1,15 +1,18 @@
-//validacion del input para el id
 const input = document.getElementById('searchInput');
-    input.addEventListener('input', function () {
-        const value = input.value.trim();
-        const numericValue = parseFloat(value);
+input.addEventListener('input', function () {
+    //validacion del input para el input del id
 
-        if (isNaN(numericValue) || numericValue < 1) {
-            input.value = ''; // Borra el contenido
-        }
-    });
+    const value = input.value.trim();
+    const numericValue = parseFloat(value);
+
+    if (isNaN(numericValue) || numericValue < 1) {
+        input.value = ''; // Borra el contenido
+    }
+});
 
 async function funcionBotonReporte(id){
+    // Función que cambia el estado del instrumento seleccionado en la base de datos
+
     // Opciones de la solicitud fetch
     let cambio = {funciona_estado: 0}
     const requestOptions = {
@@ -21,7 +24,7 @@ async function funcionBotonReporte(id){
     };
 
 
-    // habria que importar esta funcion de inventario
+    // Petición a inventario (habria que importar esta ruta de inventario)
     await fetch(`http://localhost:3000/api/general/${id}` , requestOptions)
     .then(response => {
         if (!response.ok) {
@@ -37,12 +40,11 @@ async function funcionBotonReporte(id){
         console.error('Fetch error:', error);
         // Manejar el error de la solicitud
       });
-    
-    
 }
 
 async function obtenerObjetosCocina(){
-  
+  // Función que hace una petición a inventario y retorna una lista con los instrumentos
+
     const response = await fetch(`http://localhost:3000/api/general`); /*Importar Modulo externo Inventario*/
     if (!response.ok){
         throw new Error('No se pudo obtener los objetos de gneeral');
@@ -58,34 +60,27 @@ async function obtenerObjetosCocina(){
     return instrumentosCocina
 }
 
-let reportarButton = document.querySelector(".report-button")
-reportarButton.addEventListener("click", function () {
-    let input = document.getElementById("searchInput")
-    id = parseInt(input.value)
-    funcionBotonReporte(id)
-    actualizarFilas()
-    input.value = ""
-    alert("Instrumento Resportado")
-})
-
 async function actualizarFilas() {
+  // Función que actualiza las filas de la tabla de la view
+
     let tableBody = document.getElementById("tBody")
-    tableBody.innerHTML = ""
+    tableBody.innerHTML = "" // Se eliminan las filas
     
     let instrumentos = obtenerObjetosCocina()
     instrumentos.forEach(instrumento => {
+      // Por cada instrumento se agrega una fila al tbody de la tabla
+
+      // Se crean las celdas con la información del instrumento
       let filaTabla = document.createElement("tr")
       filaTabla.className = "table-row" 
 
       let celdaId = document.createElement("td") 
       celdaId.className = "row-cell" 
       celdaId.textContent = `${instrumento.id}`
-      filaTabla.appendChild(celdaId)
 
       let celdaNombre = document.createElement("td") 
       celdaNombre.className = "row-cell-name" 
       celdaNombre.textContent = `${instrumento.nombre}`
-      filaTabla.appendChild(celdaNombre)
 
       let celdaFunciona = document.createElement("td") 
       celdaFunciona.className = "row-cell"
@@ -95,14 +90,29 @@ async function actualizarFilas() {
       else if (instrumento.funciona_estado === 0) {
         celdaFunciona.textContent = `No funciona`
       }
+      // Se agregan las celdas con los datos a la fila
+      filaTabla.appendChild(celdaId)
+      filaTabla.appendChild(celdaNombre)
       filaTabla.appendChild(celdaFunciona)
 
-      tableBody.appendChild(filaTabla)
+      tableBody.appendChild(filaTabla) // Se agrega la fila al tbody
     })
 }
 
+let reportarButton = document.querySelector(".report-button")
+reportarButton.addEventListener("click", function () {
+  // Función que toma el id del input, reporta el fallo del instrumento y muestra los cambios en la view 
+
+    let input = document.getElementById("searchInput")
+    id = parseInt(input.value)
+    funcionBotonReporte(id)
+    actualizarFilas()
+    input.value = ""
+    alert("Instrumento Resportado")
+})
 
 document.addEventListener('DOMContentLoaded', function() {
+  // Se cargan las filas en la tabla después de cargarse el HTML
   actualizarFilas()
 })
 
