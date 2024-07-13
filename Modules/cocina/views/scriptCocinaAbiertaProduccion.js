b9617ee3ae351269bdb0794877695644e6d7b2b2
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2024-07-13 17:10:09 -0400

    agregando comentarios al script de cocina abierta produccion

--- Modules/cocina/views/scriptCocinaAbiertaProduccion.js
+++ Modules/cocina/views/scriptCocinaAbiertaProduccion.js
@@ -10,29 +10,44 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// funcion para actualizar en las vistas los pedidos (objetos factura de ventas) en las vistas
 async function actualizarPedidos() {
     try {
         // Metodo que verifica si hay pedidos "pendientes" por procesar, y se muestran en la view.
 
-        // esta ruta hay que importarla de ventas, no es la ruta definitiva
-        const response = await fetch('http://localhost:3000/ventas/factura'); // Peticion a ventas que retorna el pedido
+        // esta ruta hay que importarla de ventas,
+        const response = await fetch('http://localhost:3000/ventas/factura'); 
         if (!response.ok) {
             throw new Error('No se pudo obtener la lista de pedidos');
         }
-        const pedidos = await response.json();
+        const pedidos = await response.json(); // aqui se guardaria un arreglo de todos los pedidos(facturas) almacenados por ventas
 
-        // Iterar sobre los pedidos y actualizar las order-cards
+        // Iterar sobre los pedidos y actualizar los order-card
         pedidos.forEach(pedido => {
+
+            // si el status_pedido es igual a 1, entonces es una orden pendiente
             if (pedido.status_pedido == 1) {
+
+                // seleccionamos todas las order-card (los contenedores con los pedidos que se muestrabn en las views)
                 let cardsMesas = document.querySelectorAll(".order-card")
+
+                // iteramos cada contenedor
                 for (const cardMesa of cardsMesas) {
+
+                    // si el contenedor no esta ocupado por otro pedido
                     if (!cardMesa.classList.contains("ocupado")) {
-                        cardMesa.id = pedido.id.toString();
+
+                        // al id del contenedor le asignamos el id del pedido
+                        cardMesa.id = pedido.id_cliente.toString();
+                        // al contenedor le agregamos la clase de ocupado
                         cardMesa.classList.add("ocupado");
+
+                        //seecionamos el ultimo hijo que es el que muestra el texto del pedido en las views
                         let carMesaStatus = cardMesa.lastElementChild;
-                        carMesaStatus.textContent = `ID pedido: ${pedido.id} \nEstatus: ${pedido.status}`;
-                        encontrado = true;
-                        break; // Detener el bucle una vez que se haya encontrado y actualizado una cardMesa
+                        
+                        //Le colocamos el texto al contenedor del pedido
+                        carMesaStatus.innerHTML = `ID pedido: ${pedido.id_cliente} <br>Estatus: pendiente`;
+                        break; 
                     }
                 }
             }
@@ -42,14 +57,15 @@ async function actualizarPedidos() {
         console.error('Error al actualizar los pedidos:', error);
     }
 }
-
-
-actualizarPedidos(); // Llamar a la función para actualizar los pedidos inicialmente
-setInterval(actualizarPedidos, 15000); // Actualizar cada 15 segundos (15000 milisegundos)
+setInterval(actualizarPedidos, 15000); // Actualizar la vista de los pedidos cada 15 segundos (15000 milisegundos)
 
 //boton procesar
-var idPedido = null // Se declara de forma global para usarse en las siguientes funciones (Por lo que su valor cambia)
+var idPedido = null // Esta variable guarda los id de los contenedores de pedidos de que se seleccionen en las vistas que a su vez, coinciden con los id de los pedidos que guarda ventas en la base de datos
+
+// obtenemos el boton procesar
 let botonProcesar = document.querySelector("#botonProcesar")
+
+// le añadimos un evento a este boton procesar
 botonProcesar.addEventListener("click" , async () => {
     // Evento asignado al botón de "Procesar". Se encarga de validar si se tienen ingredientes e instrumentos disponibles
 
@@ -57,26 +73,39 @@ botonProcesar.addEventListener("click" , async () => {
         alert("Seleccione un pedido")
         return null
     }
-    let idCardSeleccionada = idPedido 
+
+    let idCardSeleccionada = idPedido // se guarda el id del pedido seleccionado
+
+    // hacemos una peticion a este endpoint (que es de cocina, que a su vez usa internamente un endpoint de ventas) para que verifique si se puede hacer el pedido cuyo id es el que le pasamos por la url, y ademas este endpoint retorna ese pedido
     let response = await fetch(`http://localhost:1234/comidas/procesar-pedido?pedido_id=${idCardSeleccionada}`) // Petición que lleva al controlador de cocina
-    let pedido = await response.json() // Se toma el pedido retornado por el controlador, el cual hace la validación y cambia el estado del pedido
+
+    let pedido = await response.json() // Se toma el pedido retornado por el controlador, el ya valido si se puede realizar la comida y se sabe accediendo a su atributo de estatus
+
+    // si el estatus pedido es 3, entonces se acepta el pédido, y se puede preparar las comidas/bebidas
     if (pedido.status_pedido == 3){
+
+        // se obtiene el contenedor del pedido seleccionado
         let cardMesa = document.querySelector(`#${idCardSeleccionada}`)
         let cardMesaStatus = cardMesa.lastElementChild;
-        cardMesaStatus.textContent = `ID pedido: ${idCardSeleccionada} \nEstatus: Cocinando`;
-        // Al aceptarse el pedido se considera (en la view) que inicia la cocción 
+        cardMesaStatus.innerHTML = `ID pedido: ${idCardSeleccionada} <br>Estatus: preparando`;
+        // Al aceptarse el pedido se considera (en la view) que inicia la preparacion 
     }
-    else if(pedido.status == 2){
+
+    // si el estatus del pedido es 2, entonces se rechaza el pedido porque no se cuentan con los recursos para procesarlo
+    else if(pedido.status_pedido == 2){
         alert("No se cuenta con los recursos para realizar este pedido")
         let cardMesa = document.querySelector(`#${idCardSeleccionada}`)
         let cardMesaStatus = cardMesa.lastElementChild;
-        cardMesaStatus.textContent = `ID pedido: ${idCardSeleccionada} \nEstatus: Rechazado`;
+        cardMesaStatus.innerHTML = `ID pedido: ${idCardSeleccionada} \nEstatus: Rechazado`;
         // Al rechazarse Ventas debe manejar el pedido
     }
 })
 
+
+// aqui le damos la funcionalidad a los contenedores de los pedidos cuando se le den click
 const pedidosContainers = document.querySelectorAll(".order-card") //cartas de pedidos
 pedidosContainers.forEach(container => {
+
     container.addEventListener("click", async function (e) {
         // Evento que muestra detalles del pedido, en la carta resumen, al clickear una de las cartas de pedidos
 
@@ -90,6 +119,7 @@ pedidosContainers.forEach(container => {
         e.target.classList.toggle("clicked") 
 
 
+        // al id del contenedor que coincide con el id del pedido, se lo asignamos al variable global que guarda los id
         let idStringCardSeleccionada = e.target.id
         idPedido = idStringCardSeleccionada // Obtenemos el id del pedido
 
@@ -104,8 +134,8 @@ pedidosContainers.forEach(container => {
         .then(data => {
             // Se muestran los datos del pedido en la carta resumen
 
-            let pedidos = data
-            let comidasPedido = JSON.parse(pedidos.consumo) // Obtenemos el diccionario con los id de los platos y la cantidad
+            let pedido = data
+            let comidasPedido = JSON.parse(pedido.consumo) // Obtenemos el diccionario con los id de los platos y la cantidad
 
             let contenedorPedido = document.querySelector(".order-summary")
             contenedorPedido.innerHTML = "" // Se borra el contenido de la carta resumen de pedidos
@@ -155,19 +185,23 @@ pedidosContainers.forEach(container => {
     })
 })
 
+
+// accedemos al boton listo
 const listoButton = document.querySelector(".order-ready")
+
+// le agregamos el evento al boton listo
 listoButton.addEventListener("click", async function () {
     // Evento que cambia el estado del pedido
 
-    let cardSeleccionada = document.getElementById(idPedido)
-    let pedidoEstado = cardSeleccionada.textContent.split(/\s+/).pop() // Obtenemos el estado
+    // accedemos al contenedor que contiene el pedido seleccionado
+    let cardSeleccionada = document.getElementById(idPedido.toString())
 
     if (idPedido === null){
         // Se verifica que haya un pedido seleccionado
         alert("Seleccione un pedido")
         return null
     }
-    else if (!pedidoEstado === "Cocinando"){
+    else if (!cardSeleccionada.innerHTML.includes("preparando")){
         // Se verifica que el pedido se haya aceptado antes de cambiar su estado a listo
         alert("Primero debe procesar el pedido")
         return null
@@ -184,13 +218,14 @@ listoButton.addEventListener("click", async function () {
         let cardPedido = document.getElementById(`${idPedido}`)
         cardPedido.classList.remove("ocupado")
         let cardPedidoStatus = cardPedido.lastElementChild;
-        cardPedidoStatus.textContent = `ID pedido: ${idPedido} \nEstatus: Listo`; // Se muestra el cambio en la view
+        cardPedidoStatus.innerHTML = `ID pedido: ${idPedido}<br>Estatus: listo`; // Se muestra el cambio en la view
     })
     .catch(e => {
         console.error(`No se ha encontrado un pedido en la base de datos con el ID dado.`, e)
     })
     })
 
+// le agregamos un evento al boton devolver, que sirve para quitar de las vistas las ordenes rechazdas
 let devolverBoton = document.querySelector("#botonDevolver")
 devolverBoton.addEventListener("click" , async ()=>{
     // Evento que elimina de la carta seleccionada un pedido
@@ -199,6 +234,13 @@ devolverBoton.addEventListener("click" , async ()=>{
         alert("Seleccione un pedido")
         return null
     }
+    let cardSeleccionada = document.getElementById(idPedido.toString())
+
+    // si el pedido no ha sido rechazado entonces no se puede devolver
+    if (!cardSeleccionada.innerHTML.includes("rechazado")){
+        alert("Solo se pueden devolver pedidos que hayan sido rechazados")
+    }
+
     let cardPedido = document.getElementById(`${idPedido}`)
     cardPedido.classList.remove("ocupado")
     let cardPedidoStatus = cardPedido.lastElementChild;
@@ -206,27 +248,38 @@ devolverBoton.addEventListener("click" , async ()=>{
 })
 
 
-// evento boton hacer de nuevo
+// evento boton hacer de nuevo en caso de que haya habido un incidente en la cocina
 let botonHacerNuevo = document.querySelector("#botonHacerNuevo")
 botonHacerNuevo.addEventListener("click" , async ()=> {
-    // El mismo código del evento del botón procesar xd
 
     if (idPedido == null){
         alert("Seleccione un pedido")
         return null
     }
-    let idCardSeleccionada = idPedido 
-    let response = await fetch(`http://localhost:1234/comidas/procesar-pedido?pedido_id=${idCardSeleccionada}`)
-    let pedido = await response.json()
-    if (pedido.status == 3){
+
+    let idCardSeleccionada = idPedido // se guarda el id del pedido seleccionado
+
+    // hacemos una peticion a este endpoint (que es de cocina, que a su vez usa internamente un endpoint de ventas) para que verifique si se puede hacer el pedido cuyo id es el que le pasamos por la url, y ademas este endpoint retorna ese pedido
+    let response = await fetch(`http://localhost:1234/comidas/procesar-pedido?pedido_id=${idCardSeleccionada}`) // Petición que lleva al controlador de cocina
+
+    let pedido = await response.json() // Se toma el pedido retornado por el controlador, el ya valido si se puede realizar la comida y se sabe accediendo a su atributo de estatus
+
+    // si el estatus pedido es 3, entonces se acepta el pédido, y se puede preparar las comidas/bebidas
+    if (pedido.status_pedido == 3){
+
+        // se obtiene el contenedor del pedido seleccionado
         let cardMesa = document.querySelector(`#${idCardSeleccionada}`)
         let cardMesaStatus = cardMesa.lastElementChild;
-        cardMesaStatus.textContent = `ID pedido: ${idCardSeleccionada} \nEstatus: Cocinando`;
+        cardMesaStatus.innerHTML = `ID pedido: ${idCardSeleccionada} <br>Estatus: preparando`;
+        alert("Se cuenta con los recursos para volver a preparar el pedido") 
     }
-    else if(pedido.status == 2){
-        alert("No se cuenta con los recursos para realizar este pedido")
+
+    // si el estatus del pedido es 2, entonces se rechaza el pedido porque no se cuentan con los recursos para procesarlo
+    else if(pedido.status_pedido == 2){
+        alert("No se cuenta con los recursos para realizar de nuevo este pedido")
         let cardMesa = document.querySelector(`#${idCardSeleccionada}`)
         let cardMesaStatus = cardMesa.lastElementChild;
-        cardMesaStatus.textContent = `ID pedido: ${idCardSeleccionada} \nEstatus: Rechazado`;
+        cardMesaStatus.innerHTML = `ID pedido: ${idCardSeleccionada} \nEstatus: Rechazado`;
+        // Al rechazarse Ventas debe manejar el pedido
     }
 })
