38bd3e796d8086f421d67e02210061577c15114a
VickyMVGL <mariaviclg2005@gmail.com>
2024-07-13 22:15:45 -0400

    eliminacion pagina personal de la conexion

--- Modules/mantenimiento/scripts/Conexion_pagina.js
+++ Modules/mantenimiento/scripts/Conexion_pagina.js
@@ -1,13 +1,8 @@
 const navegar_contacto = document.getElementById('Page_contactos');
-const navegar_personal = document.getElementById('Page_personal');
 const navegar_reportes = document.getElementById('Page_reportes');
 
 navegar_contacto.addEventListener('click',function(){
     window.location.href = 'Pagina_contacto.html';
-})
-
-navegar_personal.addEventListener('click',function(){
-    window.location.href = 'Pagina_personal.html';
 })
 
 navegar_reportes.addEventListener('click',function(){
