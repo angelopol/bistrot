79549eb41212262ff7d1edb3e2224df8b3076afa
Sperrotta10 <santiagoperrotta10@gmail.com>
2024-07-16 23:49:40 -0400

    cambios en conexiones de las base de datos con los fetch y rutas, funcionalidades

--- Modules/ventas/routes/ventas.js
+++ Modules/ventas/routes/ventas.js
@@ -18,28 +18,28 @@ export const createVentasRouter = () => {
   VentasRouter.get('/caja', controllerCaja.getAll_c);
   VentasRouter.get('/caja/:id', controllerCaja.getById_c);
   VentasRouter.post('/caja', controllerCaja.create_c);
-  VentasRouter.put('/:id', controllerCaja.update_c);
+  VentasRouter.patch('/:id', controllerCaja.update_c);
   VentasRouter.delete('/caja/:id', controllerCaja.delete_c);
 
   // Rutas de Salón
   VentasRouter.get('/salon', controllerSalon.getAll_s);
   VentasRouter.get('/salon/:id', controllerSalon.getById_s);
   VentasRouter.post('/salon', controllerSalon.create_s);
-  VentasRouter.put('/salon/:id', controllerSalon.update_s);
+  VentasRouter.patch('/salon/:id', controllerSalon.update_s);
   VentasRouter.delete('/salon/:id', controllerSalon.delete_s);
 
   // Rutas de Cliente
   VentasRouter.get('/cliente', controllerCliente.getAll_cl);
   VentasRouter.get('/cliente/:id', controllerCliente.getById_cl);
   VentasRouter.post('/cliente', controllerCliente.create_cl);
-  VentasRouter.put('/cliente/:id', controllerCliente.update_cl);
+  VentasRouter.patch('/cliente/:id', controllerCliente.update_cl);
   VentasRouter.delete('/cliente/:id', controllerCliente.delete_cl);
 
   // Rutas de Factura
   VentasRouter.get('/factura', controllerFactura.getAll_f);
   VentasRouter.get('/factura/:id', controllerFactura.getById_f);
   VentasRouter.post('/factura', controllerFactura.create_f);
-  VentasRouter.put('/factura/:id', controllerFactura.update_f);
+  VentasRouter.patch('/factura/:id', controllerFactura.update_f);
   VentasRouter.delete('/factura/:id', controllerFactura.delete_f);
 
   //Renderizado de Vistas
