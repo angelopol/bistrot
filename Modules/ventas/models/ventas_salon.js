ac39897adc9f0fa3a635c8e4436381d1610e39d5
angelopol <114349306+angelopol@users.noreply.github.com>
2024-07-20 00:35:12 -0400

    update ventas_Salon

--- Modules/ventas/models/ventas_salon.js
+++ Modules/ventas/models/ventas_salon.js
@@ -3,11 +3,11 @@ import 'dotenv/config'
 //import {promisify} from 'util';
 
 const db = {
-    host: "localhost",
-    user: "root",
-    port: 3306,
-    password: "$0p0rt3",
-    database: "bistrot",
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    port: process.env.DB_PORT || 3306,
+    password: process.env.DB_PASSWORD || '$0p0rt3',
+    database: process.env.DB_DATABASE || 'bistrot', 
 };
 
 
