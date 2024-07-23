21d6c264aa01f10e6ab90e91874499b8fd2f7e1d
L1nkStart <eduar06tae@gmail.com>
2024-07-22 23:11:08 -0400

    cambios

--- global/TablesScheme/reserva.sql
+++ global/TablesScheme/reserva.sql
@@ -8,7 +8,7 @@ CREATE TABLE `mesas` (
 
 CREATE TABLE `reserva` (
   `numero_reserva` int NOT NULL AUTO_INCREMENT,
-  `ID_mesa` int NOT NULL DEFAULT '0',
+  `ID_mesa` varchar(45) NOT NULL DEFAULT '0',
   `confirmado` tinyint DEFAULT NULL,
   `cantidad_personas` int NOT NULL,
   `ID_cliente` int NOT NULL ,
@@ -18,7 +18,6 @@ CREATE TABLE `reserva` (
   `descripcion` text NOT NULL,
   `telefono` varchar(45) NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
   `ubicacion` varchar(500) NOT NULL,
   PRIMARY KEY (`numero_reserva`)
 ) 
