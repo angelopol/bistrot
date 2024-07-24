7ad89dfc51f215673dace2330ab6ca84d38bf5e0
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2024-07-24 09:51:18 -0400

    quitando commits de cocina en master

---
+++ views/cocina/scriptCrearPlato.js
@@ -0,0 +1,271 @@
+// Script para el funcionamiento del formulario
+
+
+window.onload = function () {
+    const maxIngredients = 4;
+    let proteinCount = 1;
+    let verdurasCount = 1;
+    let otroTipoCount = 1;
+    let instrumentosCount = 1;
+
+    document.getElementById("addproteina").addEventListener("click", () => {
+        if (proteinCount < maxIngredients) {
+            proteinCount++;
+            document.getElementById("Proteinas").insertAdjacentHTML('beforeend', `
+                <div class="ingredient-group">
+                    <select class="form-input dropdown text" name="Proteina">
+                        <option value="" disabled selected>Proteina</option>
+                                                <option value="7">Pollo</option>
+                                                <option value="1">Queso</option>
+                                                <option value="24">Huevo</option>
+                                                <option value="15">Bacalao</option>
+                    </select>
+                    <input type="number" class="form-input" name="PesoProteina" placeholder="Cantidad"  style = "width: 50px"  />
+                    <button type="button" class="remove-button">-</button>
+                </div>
+            `);
+        }
+    });
+
+    document.getElementById("addverduras").addEventListener("click", () => {
+        if (verdurasCount < maxIngredients) {
+            verdurasCount++;
+            document.getElementById("Verduras").insertAdjacentHTML('beforeend', `
+                <div class="ingredient-group">
+                    <select class="form-input dropdown text" name="Verduras">
+                        <option value="" disabled selected>Verduras</option>
+                                                <option value="2">Tomate</option>
+                                                <option value="3">Zanahoria</option>
+                                                <option value="4">Cebolla</option>
+                                                <option value="5">Papa</option>
+                                                <option value="6">Apio</option>
+                                                <option value="8">Ajo</option>
+                                                <option value="9">Champiñones</option>
+                                                <option value="13">Berenjena</option>
+                                                <option value="20">Fresa</option>
+                                                <option value="21">Patilla</option>
+                                                <option value="22">Manzana</option>
+                                                <option value="23">Banana</option>
+                                                <option value="26">Limon</option>
+                                                <option value="38">Lechuga</option>
+                                                <option value="39">Pepino</option>
+                                                <option value="40">Aji</option>
+                                                <option value="41">Pimenton</option>
+                                                <option value="42">Cebollin</option>
+                    </select>
+                    <input type="number" class="form-input" name="PesoVerduras" placeholder="Cantidad" style = "width: 50px" />
+                    <button type="button" class="remove-button">-</button>
+                </div>
+            `);
+        }
+    });
+
+    document.getElementById("addotrotipo").addEventListener("click", () => {
+        if (otroTipoCount < maxIngredients) {
+            otroTipoCount++;
+            document.getElementById("OtroTipo").insertAdjacentHTML('beforeend', `
+                <div class="ingredient-group">
+                    <select class="form-input dropdown text" name="OtroTipo">
+                        <option value="" disabled selected>Otros</option>
+                                                <option value="10">Espelta</option>
+                                                <option value="11">Vinagre</option>
+                                                <option value="12">Pasta</option>
+                                                <option value="14">Aceite</option>
+                                                <option value="16">Harina</option>
+                                                <option value="17">Leche</option>
+                                                <option value="18">Azucar</option>
+                                                <option value="19">Chocolate</option>
+                    </select>
+                    <input type="number" class="form-input" name="PesoOtros" placeholder="Cantidad" style = "width: 50px" />
+                    <button type="button" class="remove-button">-</button>
+                </div>
+            `);
+        }
+    });
+
+    document.getElementById("addinstrumento").addEventListener("click", () => {
+        if (instrumentosCount < maxIngredients) {
+            instrumentosCount++;
+            document.getElementById("Instrumentos").insertAdjacentHTML('beforeend', `
+                <div class="instrument-group">
+                    <select class="form-input dropdown text" name="Instrumentos">
+                        <option value="" disabled selected>Instrumentos</option>
+                                                    <option value="1">cocinaA</option>
+                                                    <option value="2">cocinaB</option>
+                                                    <option value="3">cocinaC</option>
+                                                    <option value="4">cocinaD</option>
+                                                    <option value="5">hornoA</option>
+                                                    <option value="6">hornoB</option>
+                                                    <option value="7">hornoC</option>
+                                                    <option value="8">hornoD</option>
+                                                    <option value="9">SartenA</option>
+                                                    <option value="10">SartenB</option>
+                                                    <option value="11">SartenC</option>
+                                                    <option value="12">ColadorA</option>
+                                                    <option value="13">ColadorB</option>
+                                                    <option value="14">OllaA</option>
+                                                    <option value="15">OllaB</option>
+                                                    <option value="16">OllaC</option>
+                                                    <option value="17">NeveraA</option>
+                                                    <option value="18">NeveraB</option>
+                                                    <option value="19">CocteleraA</option>
+                                                    <option value="20">CocteleraB</option>
+                                                    <option value="21">CocteleraC</option>
+                                                    <option value="22">CocteleraD</option>
+                                                    <option value="23">BatidoraA</option>
+                                                    <option value="24">BatidoraB</option>
+                    </select>
+                    <button type="button" class="remove-button">-</button>
+                </div>
+            `);
+        }
+    });
+
+    document.addEventListener("click", (event) => {
+        if (event.target.classList.contains("remove-button")) {
+            const ingredientGroup = event.target.closest(".ingredient-group");
+            if (ingredientGroup) {
+                ingredientGroup.remove();
+                proteinCount--;
+                verdurasCount--;
+                otroTipoCount--;
+            } else {
+                const instrumentGroup = event.target.closest(".instrument-group");
+                if (instrumentGroup) {
+                    instrumentGroup.remove();
+                    instrumentosCount--;
+                }
+            }
+        }
+    });
+    
+    
+    // Script backend
+    let botonRegistrar = document.querySelector("#submit-button")
+    console.log(botonRegistrar)
+    
+    botonRegistrar.addEventListener("click",async ()=> {
+        // validaciones 
+        let comprobacionProteina = document.querySelector("#Proteina")
+        let comprobacionVerdura = document.querySelector("#Verdura")
+        let comprobacionOtro = document.querySelector("#OtroTipoX")
+        let comprobacionTipo = document.querySelector("#TipoX")
+        let comprobacionInstrumento = document.querySelector("#Instrumento")
+        let comprobacionNombre = document.querySelector("#preparacion")
+        if(comprobacionProteina.value == "" || comprobacionVerdura.value == "" || comprobacionOtro.value == "" || comprobacionTipo.value == "" || comprobacionInstrumento.value == "" || comprobacionNombre.value == ""){
+            return alert("No deje campos sin seleccionar")
+        }
+
+        //"form-input dropdown text"
+        let selectIngredientes = document.querySelectorAll(".form-input.dropdown.text")
+        let nombreIngredientes = []
+        // Tipo o Instrumentos
+        selectIngredientes.forEach(selectIngrediente => {
+            if(selectIngrediente.name !== "Tipo" && selectIngrediente.name !== "Instrumentos") {
+                nombreIngredientes.push(selectIngrediente.value)
+                console.log(typeof selectIngrediente.value)
+                console.log(selectIngrediente.value)
+            }
+        })
+    
+       // form-input
+       
+       let selectCantidades = document.querySelectorAll(".form-input")
+       let cantidades = []
+       selectCantidades.forEach(selectCantidad => {
+            if(selectCantidad.type == "number"){
+            cantidades.push(selectCantidad.value)
+            console.log(selectCantidad.value)
+            console.log(typeof selectCantidad.value)
+            }
+        })
+
+        console.log(nombreIngredientes)
+        console.log(cantidades)
+
+        let ingredientes = crearFormatoJSON(nombreIngredientes , cantidades) // {"12":21,"25":32,"34":47}
+
+        // sacamos el tipo de comida
+        let selectTipos = document.querySelectorAll(".form-input")
+        let tipoComida = null
+        selectTipos.forEach(selectTipo => {
+            if(selectTipo.name == "Tipo"){
+                tipoComida = selectTipo.value
+            }
+        })
+        
+        console.log(tipoComida)
+
+        // sacamos los intumentos 
+        let selectInstrumentos = document.querySelectorAll(".form-input")
+        let instrumentosLista = []
+        selectInstrumentos.forEach(selectInstrumento => {
+            if(selectInstrumento.name == "Instrumentos"){
+                instrumentosLista.push(selectInstrumento.value)
+            }
+        })
+
+        let instrumentosString = instrumentosLista.join(",")
+        
+        // estado de seleccionada por defecto en false
+        let seleccionada = 0
+
+        // sacamos el nombre
+        let nombre = document.querySelector("#preparacion").value
+
+        // hacemos la solicitud a nuestro endpoint para crear la nueva comida
+
+        console.log(ingredientes)
+        const solicitud = {
+            nombre: nombre,
+            tipo_comida: tipoComida,
+            tipo_bebida: null,
+            instrumentos: instrumentosString,
+            ingredientes: ingredientes,
+            seleccionada: 0
+        }
+
+        console.log({solicitud})
+
+        try{
+            await fetch('/cocina', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(solicitud)
+            });
+            console.log("Comida creada")
+        }
+        catch(e){
+            console.log(`Error: `, e)
+        }
+        alert("Comida creada correctamente")
+        location.reload(true)
+    })
+
+    
+    
+};
+
+function crearFormatoJSON(arr1, arr2) {
+    // Verificar que ambos arreglos tengan la misma longitud
+    if (arr1.length !== arr2.length) {
+        throw new Error('Los arreglos deben tener la misma longitud');
+    }
+
+    const objetoJSON = {};
+    // Iterar sobre los arreglos
+    for (let i = 0; i < arr1.length; i++) {
+        const key = arr1[i];
+        const value = parseFloat(arr2[i]); // Parsear el valor para asegurarse de que sea numérico si es posible
+        objetoJSON[key] = value;
+    }
+    return JSON.stringify(objetoJSON);
+}
+
+
+// Ejemplo de uso:
+const arr1 = ["12", "25", "34"];
+const arr2 = ["21", "32", "47"];
+const resultado = crearFormatoJSON(arr1, arr2);
+console.log(resultado); // Output: '{"1":2,"2":3,"3":4}'¿
+
---
+++ views/cocina/scriptPlanReservas.js
@@ -0,0 +1,224 @@
+var reservaID = ""
+
+async function obtenerReservas() {
+    let reservas = []
+
+    const options = {
+        method: 'GET', 
+        headers: {
+        'Content-Type': 'application/json' 
+        }
+    };
+    await fetch(`/reservas/obtener-reservas`, options)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); 
+      })
+    .then(data => {
+        reservas = data
+        console.log("Se han obtenido exitosamente las reservas: ", reservas)
+    })
+    .catch(error => {
+        console.error("Error al obtener las reservas: ", error)
+    })
+    return reservas
+}
+
+async function obtenerNombrePlatoPorID(id) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    
+    try {
+        const response = await fetch(`/cocina/comida/${id}`, options);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log("Se ha obtenido exitosamente el plato: ", data.nombre);
+        let plato = data[0].nombre
+        return plato;
+    } catch (error) {
+        console.error("Error al obtener las recetas: ", error);
+    }
+}
+
+async function cargarPlatos() {
+    const reservas = await obtenerReservas()
+
+    const tbody = document.getElementById("tbody")
+    tbody.innerHTML = ""
+    
+    for (const reserva of reservas) {
+        if (reserva.preferencias !== "") {
+            const row = document.createElement("tr")
+            row.className = "table-row" 
+            row.id = `${reserva.numero_reserva}`
+    
+            let celdaId = document.createElement("td") 
+            celdaId.className = "row-cell"
+            celdaId.textContent = `${reserva.numero_reserva}`
+    
+            let celdaFecha = document.createElement("td") 
+            celdaFecha.className = "row-cell-name" 
+            celdaFecha.textContent = `${reserva.fecha.split("T")[0]}`
+    
+            let celdaTipo = document.createElement("td") 
+            celdaTipo.className = "row-cell" 
+    
+            let numeroMesas = (reserva.ID_mesa).split(", ")
+    
+            if (numeroMesas.length === 16) {
+                celdaTipo.textContent = `Completa`
+            }
+            else if (numeroMesas.length < 16) {
+                celdaTipo.textContent = `Individuales`
+            }
+            row.appendChild(celdaId)
+            row.appendChild(celdaFecha)
+            row.appendChild(celdaTipo)
+            row.addEventListener("click", async function (e) {
+                let filas = document.querySelectorAll('.table-row'); 
+                filas.forEach(filaAux => {
+                    if (!(filaAux == e.currentTarget)){
+                        filaAux.classList.remove("clicked")
+                    }
+                })
+                e.currentTarget.classList.toggle("clicked")
+                reservaID = e.currentTarget.id
+                let reservas = await obtenerReservas()
+                let reserva = null
+    
+                for (let reservaAux of reservas) {
+                    if (reservaAux.numero_reserva == reservaID) {
+                        reserva = reservaAux
+                        break
+                    }
+                }
+                const platos = JSON.parse(reserva.preferencias)
+                const platosDetalles = document.getElementById("input-ingre")
+                
+                let platosAux = ""
+                console.log(reserva.preferencias)
+                for (const plato in platos) {
+                    console.log(plato)
+                    let platoNombre = await obtenerNombrePlatoPorID(plato)
+                    platosAux = `${platosAux}${platoNombre} ${platos[plato]}x. `
+                }
+                platosDetalles.innerText = platosAux
+        
+                console.log("Se han obtenido exitosamente los platos de la reservación: ", reservaID)
+            })
+            tbody.appendChild(row)
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", cargarPlatos()) // Solo se cargan una vez
+
+let botonIngredientes = document.querySelector("#btnIngredientes")
+botonIngredientes.addEventListener("click", async ()=> {
+    if(reservaID == ""){
+        return alert("Seleccione una reserva")
+    }
+    let reservas = await obtenerReservas()
+    let reserva = null
+    reservas.forEach(objetoReserva => {
+        if(objetoReserva.numero_reserva == reservaID){
+            reserva = objetoReserva
+        }
+    })
+    let numeroMesas = reserva.ID_mesa.split(",")
+    if(numeroMesas.length > 16){
+        return alert("Solo se compran ingredientes con reservas individuales")
+    }
+
+    // Hacemos que las recetas que haya reservado el comensal se carguen en el menu del dia
+    let preferencias = JSON.parse(reserva.preferencias)
+    let idPlatos = Object.keys(preferencias) // lista con los id de los platos seleccionados
+    let cantidadesPlatos = Object.values(preferencias)
+
+    for (let platoID in idPlatos) {
+        const cambios = {
+            "seleccionada": 1
+        }
+        const options = {
+            method: 'PATCH', 
+            headers: {
+            'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(cambios)
+        };
+        await fetch(`/cocina/comida/${idPlatos[platoID]}`, options)
+        .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json(); 
+          })
+        .then(data => {
+            console.log(`El plato de ID ${idPlatos[platoID]} ha sido agregado al menú del día`)
+        })
+        .catch(error => {
+            console.error("Error al agregar la receta al menu del dia: ", error)
+        })
+    }
+
+    // obtenemos las comidas
+
+    let fetchPromises = idPlatos.map(async idPlato => {
+        let res = await fetch(`/cocina/comida/${idPlato}`)
+        let nombrePlato = await res.json();
+        return nombrePlato[0]; // objeto comida
+    })
+
+    let comidas = await Promise.all(fetchPromises); // array con los objetos comidas
+
+    let ingredientesRequeridos = {}
+        for(let i = 0; i < comidas.length ; i++){
+            let ingredientesParaComida = JSON.parse(comidas[i].ingredientes) // {"2":1}
+            
+            Object.keys(ingredientesParaComida).forEach(idIngredienteRequerido => {
+                if (idIngredienteRequerido in ingredientesRequeridos){
+                    ingredientesRequeridos[idIngredienteRequerido] += parseFloat(parseFloat(ingredientesParaComida[idIngredienteRequerido]) * parseInt(cantidadesPlatos[i]))
+                }
+                else{
+                    ingredientesRequeridos[idIngredienteRequerido] = parseFloat(parseFloat(ingredientesParaComida[idIngredienteRequerido]) * parseInt(cantidadesPlatos[i]))
+                }
+            })
+    }
+    
+    Object.keys(ingredientesRequeridos).forEach(async idIngredienteRequerido => {
+        let nombreIngrediente = ""
+        let ingrediente = await fetch(`/inventario/api/cocina-bar/${idIngredienteRequerido}`)
+        ingrediente = await ingrediente.json()
+        nombreIngrediente = ingrediente.nombre
+        const solicitud = {
+            depar: 'cocina', // departamento que realiza la solicitud
+            id_emp: "1", // ID del empleado que realiza la solicitud, ajustar según corresponda
+            cant: Math.ceil(ingredientesRequeridos[idIngredienteRequerido]).toString(),
+            nombre_producto: nombreIngrediente,
+            fecha: new Date(),
+            detalle: 'Solicitud por falta de insumo para producior un pedido'
+        };
+        // Enviar solicitud de compra
+        try{
+            await fetch('/compras-index/soli', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(solicitud)
+            });
+            console.log("solicitud enviada")
+        }
+        catch(e){
+            console.log(`Error: `, e)
+        }
+    })
+
+    alert("Solicitud para comprar los ingredientes para la reserva enviada correctamente")
+})
