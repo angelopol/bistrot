e8f09cb80fdd1739820f7956b6db5c472e08df4d
VickyMVGL <mariaviclg2005@gmail.com>
2024-07-23 21:41:17 -0400

    Cambio de la tabla sql

--- global/TablesScheme/mantenimiento.sql
+++ global/TablesScheme/mantenimiento.sql
@@ -7,11 +7,11 @@ fecha_final datetime,
 PRIMARY KEY(id_mantenimiento)
 ) ;
 
-create table if not exists contactos(
-id_contacto int not null,
-nombre varchar(45) not null,
-servicio varchar (45) not null, 
-telefono varchar(45),
-correo varchar (100),
+create table if not exists reporte(
+id_reporte int not null,
+asunto varchar(100) not null,
+descripcion varchar (700) not null, 
+origen varchar(45),
+destino varchar (45),
 Primary Key (id_contacto)
 );
